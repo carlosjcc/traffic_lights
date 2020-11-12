a9c1e1d6e6e13411a5f618938e46b66b657228ea
carlos <carloscorralesch@gmail.com>
2020-11-12 20:50:25 +1100

    change to a single output function for the fsm

--- fsm.c
+++ fsm.c
@@ -17,35 +17,41 @@
 
 void go_east(void){
     GPIO_PORTB_DATA_R = 0x0C;
+    GPIO_PORTF_DATA_R = 0x02;
 }
 
 
 void wait_east(void){
     GPIO_PORTB_DATA_R = 0x14;
+    GPIO_PORTF_DATA_R = 0x02;
 }
 
 
 void go_north(void){
     GPIO_PORTB_DATA_R = 0x21;
+    GPIO_PORTF_DATA_R = 0x02;
 }
 
 
 void wait_north(void){
     GPIO_PORTB_DATA_R = 0x22;
-}
-
-void no_cars(void){
-    GPIO_PORTB_DATA_R = 0x24;
-}
-
-
-void walk(void){
-    GPIO_PORTF_DATA_R = 0x08;
-}
-
-void no_walk(void){
     GPIO_PORTF_DATA_R = 0x02;
 }
+
+
+void no_cars_dont_walk(void){
+    GPIO_PORTB_DATA_R = 0x24;
+    GPIO_PORTF_DATA_R = 0x02;
+}
+void no_cars_walk(void){
+    GPIO_PORTB_DATA_R = 0x24;
+    GPIO_PORTF_DATA_R = 0x08;
+}
+void no_cars_walk_off(void){
+    GPIO_PORTB_DATA_R = 0x24;
+    GPIO_PORTF_DATA_R = 0x00;
+}
+
 
 unsigned long read_sensors(void) {
     return GPIO_PORTE_DATA_R;
@@ -56,17 +62,17 @@ void fsm_controller(void) {
     unsigned long sensors;
     
     const state fsm[11] = {
-        {&go_east, &no_walk, 0x00FFFFFF,    {GO_EAST,GO_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST}},
-        {&wait_east, &no_walk, 0x00FFFFFF,  {GO_EAST,GO_NORTH,GO_NORTH,GO_NORTH,WALK,WALK,WALK,WALK}},
-        {&go_north, &no_walk, 0x00FFFFFF,   {GO_NORTH,WAIT_NORTH,GO_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH}},
-        {&wait_north, &no_walk, 0x00FFFFFF, {GO_NORTH,GO_EAST,GO_NORTH,GO_EAST,WALK,WALK,WALK,WALK}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_1,BLINK_1,BLINK_1,WALK,BLINK_1,BLINK_1,BLINK_1}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_2,BLINK_2,BLINK_2,WALK,BLINK_2,BLINK_2,BLINK_2}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_3,BLINK_3,BLINK_3,WALK,BLINK_3,BLINK_3,BLINK_3}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_4,BLINK_4,BLINK_4,WALK,BLINK_4,BLINK_4,BLINK_4}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_5,BLINK_5,BLINK_5,WALK,BLINK_5,BLINK_5,BLINK_5}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,BLINK_6,BLINK_6,BLINK_6,WALK,BLINK_6,BLINK_6,BLINK_6}},
-        {&no_cars, &walk, 0x00FFFFFF,       {WALK,GO_EAST,GO_NORTH,GO_EAST,WALK,GO_EAST,GO_NORTH,GO_EAST}},
+        {&go_east, 0x00FFFFFF,    {GO_EAST,GO_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST,WAIT_EAST}},
+        {&wait_east, 0x00FFFFFF,  {GO_EAST,GO_NORTH,GO_NORTH,GO_NORTH,WALK,WALK,WALK,GO_NORTH}},
+        {&go_north, 0x00FFFFFF,   {GO_NORTH,WAIT_NORTH,GO_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH,WAIT_NORTH}},
+        {&wait_north, 0x00FFFFFF, {GO_NORTH,GO_EAST,GO_NORTH,GO_EAST,WALK,WALK,WALK,WALK}},
+        {&no_cars_walk, 0x00FFFFFF,       {WALK,BLINK_1,BLINK_1,BLINK_1,WALK,BLINK_1,BLINK_1,BLINK_1}},
+        {&no_cars_dont_walk, 0x00FFFFFF,    {WALK,BLINK_2,BLINK_2,BLINK_2,WALK,BLINK_2,BLINK_2,BLINK_2}},
+        {&no_cars_walk_off, 0x00FFFFFF,   {WALK,BLINK_3,BLINK_3,BLINK_3,WALK,BLINK_3,BLINK_3,BLINK_3}},
+        {&no_cars_dont_walk, 0x00FFFFFF,    {WALK,BLINK_4,BLINK_4,BLINK_4,WALK,BLINK_4,BLINK_4,BLINK_4}},
+        {&no_cars_walk_off, 0x00FFFFFF,   {WALK,BLINK_5,BLINK_5,BLINK_5,WALK,BLINK_5,BLINK_5,BLINK_5}},
+        {&no_cars_dont_walk, 0x00FFFFFF,    {WALK,BLINK_6,BLINK_6,BLINK_6,WALK,BLINK_6,BLINK_6,BLINK_6}},
+        {&no_cars_walk_off, 0x00FFFFFF,   {WALK,GO_EAST,GO_NORTH,GO_EAST,WALK,GO_EAST,GO_NORTH,GO_EAST}},
     };
 
 
@@ -80,8 +86,7 @@ void fsm_controller(void) {
         
         sensors = read_sensors();
         
-        (fsm[s].car_lights)();
-        (fsm[s].pedestrian_lights)();
+        (fsm[s].traffic_lights)();
         systick_wait(fsm[s].time);
         systick_wait(fsm[s].time);
         
--- fsm.h
+++ fsm.h
@@ -2,13 +2,9 @@
 #define __FSM_H__
 
 
-
-
-
 // Linked data structure
 typedef struct state_struct {
-    void (*car_lights)(void);
-    void (*pedestrian_lights)(void);
+    void (*traffic_lights)(void);
     unsigned long time;
     //unsigned long next[5];
     unsigned long next[8];
@@ -19,9 +15,9 @@ void go_east(void);
 void wait_east(void);
 void go_north(void);
 void wait_north(void);
-void no_cars(void);
-void walk(void);
-void no_walk(void);
+void no_cars_dont_walk(void);
+void no_cars_walk(void);
+void no_cars_walk_off(void);
 unsigned long read_sensors(void);
 void fsm_controller(void);
 
